c559e796f625518cd27b64d7b0eed6dcba4ae6d0
Hanjun <hanjun2430@daum.net>
2025-03-24 04:09:30 +0900

    [remi/#1] mission feat

---
+++ app/src/main/java/com/example/umc_8th/HappyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.umc_8th
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import umc.study.umc_8th.R
+
+class HappyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy) // 새로운 XML 파일과 연결
+    }
+}
