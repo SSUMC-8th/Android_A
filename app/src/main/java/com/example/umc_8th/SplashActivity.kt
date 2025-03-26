a17ccb3fce99c289550595167db26237d789fb4e
ChaeLeeee <cwlee.red25@gmail.com>
2025-03-27 03:21:53 +0900

    second

---
+++ app/src/main/java/com/example/umc_8th/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.example.umc_8th
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.navigation.fragment.NavHostFragment
+//import androidx.navigation.ui.NavigationUI
+//import com.example.umc_8th.databinding.ActivitiyMainBinding
+//import com.example.umc_8th.ui.theme.UMC_8thTheme
+
+
+class SplashActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        // 일정 시간 지연 이후 실행하기 위한 코드
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            // 일정 시간이 지나면 MainActivity로 이동
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/com/example/umc_8th/fragment/CalendarFragment.kt
@@ -0,0 +1,13 @@
+package com.example.umc_8th.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class HomeFragment:Fragment() {
+
+
+}
