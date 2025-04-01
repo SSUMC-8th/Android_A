9f6b1596c7a6304925d50d5f4f28c0783afc60e3
ChaeLeeee <cwlee.red25@gmail.com>
2025-04-01 23:39:03 +0900

    3rd

---
+++ app/src/main/java/com/example/umc_8th/HomeBannerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.umc_8th
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class HomeBannerAdaptor (fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    private val fragmentlist : ArrayList<Fragment> = ArrayList()
+
+    override fun getItemCount(): Int = fragmentlist.size
+
+    override fun createFragment(position: Int): Fragment = fragmentlist[position]
+
+    fun addFragment(fragment: Fragment){
+        fragmentlist.add(fragment)
+        notifyItemInserted(fragmentlist.size - 1) // viewpager 에게 추가된 fragment 를 알려줌
+    }
+}
---
+++ app/src/main/java/com/example/umc_8th/fragment/AlbumFragment1.kt
@@ -0,0 +1,7 @@
+package com.example.umc_8th.fragment
+
+import androidx.fragment.app.Fragment
+
+
+class AlbumFragment:Fragment() {
+}
