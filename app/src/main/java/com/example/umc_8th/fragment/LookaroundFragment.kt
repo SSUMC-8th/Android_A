d2852e98916c47792bf31737e7479cef4aeb154c
ChaeLeeee <cwlee.red25@gmail.com>
2025-05-20 18:11:52 +0900

    lookareound recycler

--- app/src/main/java/com/example/umc_8th/fragment/LookaroundFragment.kt
+++ app/src/main/java/com/example/umc_8th/fragment/LookaroundFragment.kt
@@ -5,15 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.umc_8th.adapter.CategoryAdapter
 import com.example.umc_8th.databinding.FragmentLookaroundBinding
 
 class LookaroundFragment:Fragment() {
     private var mBinding: FragmentLookaroundBinding? = null
+    private val binding get() = mBinding!!
+
+    private val categoryList = listOf(
+        "차트", "영상", "장르", "상황", "분위기", "오디오"
+    )
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentLookaroundBinding.inflate(inflater,container,false)
-        mBinding = binding
-        return mBinding?.root
+        mBinding = FragmentLookaroundBinding.inflate(inflater,container,false)
+        val adapter = CategoryAdapter(categoryList) { category ->
+
+        }
+
+        binding.categoryRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.categoryRecyclerView.adapter = adapter
+
+        return binding.root
+
     }
 
     override fun onDestroyView() {
